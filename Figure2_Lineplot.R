3c386820048a92cc4ba170a6f7b0534bbc46b989
Kelly Chang <kchang4@uw.edu>
2018-03-28 16:44:27 -0700

    removed in-line functions to separate script, edited scripts for this edit

--- Figure2_Lineplot.R
+++ Figure2_Lineplot.R
@@ -1,63 +1,62 @@
-####################
-# Make Figure 2
-# Shen et al. (2018)
-# BioRxiv
-####################
+# Figure2_Lineplot.R
 
 # Clear workspace
-rm(list = ls())
+rm(list = ls()) 
 
-# Set working directory here
-setwd("YourWorkingDirectory")
+# Load in packages and functions
+library(ggplot2)
+source("Women_in_Journal_Functions.R")
 
-# Read in NIH %female, with # female nature, science, and PNAS calculated
-top_journals <- read.csv("LineGraphTop3Journals.csv")
-year_range <- min(top_journals$Year):max(top_journals$Year)
+# Declare the working directories
+main_dir   <- "YourWorkingDirectory"
+figure_dir <- file.path(main_dir, "Figure2")
 
-# Filter to last author type
-last_author <- top_journals[article_type == "last_article", ]
-# last_author <- top_journals %>% filter(Type == "last_article")
+# Load in percentage female line plot data
+load(file.path(figure_dir, "Figure2_Lineplot.RData")) # journal percent
+year_range <- min(journal_percent$year):max(journal_percent$year)
 
-# plot last author time series
-last <- ggplot(data = last_author, 
-              aes(x = Year, 
-                  y = Percent, 
-                  color = journal)) +
-  geom_line() +
-  theme_bw() + 
-  scale_x_continuous(breaks = year_range) + 
-  labs(x = "Year", y = "Percent Female Last Author") +
-  theme(axis.text = element_text(size = 14), 
-        axis.title = element_text(size = 14), 
-        legend.text = element_text(size = 12), 
-        legend.title = element_text(size = 12),
-        legend.position = c(0.9, 0.8)) +
-  scale_colour_manual(name = "DATA SOURCE", 
-                      values = c("red", "grey", "green4", "blue")) +
-  ylim(0,50)
-
-last
-
-# Filter to first author
-# first_author <- top_journals %>% filter(Type == "first_article")
-first_author <- top_journals[article_type == "first_article", ]
+# Figure2a - Percent Female First Author ----------------------------------
 
 # Plot first author time series
-first <- ggplot(data = first_author, 
-               aes(x = Year, 
-                   y = Percent, 
+line_first <- ggplot(journal_percent, 
+               aes(x = year, 
+                   y = female_first, 
                    color = journal)) +
-  geom_line() +
-  theme_bw() + 
+  geom_errorbar(aes(ymin = female_first - sem_first, 
+                    ymax = female_first + sem_first), 
+                width = 0.1) + 
+  geom_line() + geom_point() + theme_bw() + 
   scale_x_continuous(breaks = year_range) + 
   labs(x = "Year", y = "Percent Female First Author") +
-  theme(axis.text = element_text(size = 14), 
-        axis.title = element_text(size = 14), 
-        legend.text = element_text(size = 12),
-        legend.title = element_text(size = 12),
-        legend.position = c(0.9, 0.2)) +
+  theme(text = element_text(size = 14), 
+        title = element_text(size = 16),
+        legend.position = c(0.85, 0.15),
+        legend.box.background = element_rect()) +
   scale_color_manual(name = "DATA SOURCE", 
                      values = c("red", "green4", "blue")) +
   ylim(0, 50) 
 
-first
+line_first
+
+# Figure 2b - Percent Female Last Author ----------------------------------
+
+# plot last author time series
+line_last <- ggplot(journal_percent, 
+               aes(x = year, 
+                   y = female_last, 
+                   color = journal)) +
+  geom_errorbar(aes(ymin = female_last - sem_last, 
+                    ymax = female_last + sem_last), 
+                width = 0.1) + 
+  geom_line() + geom_point() + theme_bw() + 
+  scale_x_continuous(breaks = year_range) + 
+  labs(x = "Year", y = "Percent Female Last Author") +
+  theme(text = element_text(size = 14), 
+        title = element_text(size = 16),
+        legend.position = c(0.85, 0.85), 
+        legend.box.background = element_rect()) +
+  scale_colour_manual(name = "DATA SOURCE", 
+                      values = c("red", "grey", "green4", "blue")) +
+  ylim(0, 50)
+
+line_last
--- PubMed_Genderize.R
+++ PubMed_Genderize.R
@@ -1,109 +1,35 @@
-####################
-# Genderize.io
-# Shen et al. (2018)
-# BioRxiv
-####################
+# PubMed_Genderize.R
+# 
+# Extracts all first names from journal CSV files to be "genderized" 
+# with the `genderizeR` package.
 
-library(dplyr)
-library(ggplot2)
-library(scales)
-library(data.table)
-library(readr)
-install.packages("devtools")
-devtools::install_github("kalimu/genderizeR")
+# Clear workspace
+rm(list = ls()) 
+
+# Load in packages and functions
 library(genderizeR)
+source("Women_in_Journal_Functions.R")
 
-setwd("YourWorkingDirectory")
+# Declare the working directories
+main_dir   <- "YourWorkingDirectory"
+figure_dir <- file.path(main_dir, "Figure1")
 
-temp<-list.files(pattern="*.csv")
+# Locate all the data .csv files
+file_names <- list.files(figure_dir, pattern = "*.csv")
+file_names <- file.path(figure_dir, file_names)
 
-##Process all the files in the folder
-##Note: Journal name might need eyeballing!!
+# Extract all first names (first and last authors)
+# Remove duplicates and empty strings
+# Convert to packaged lists of 200 names to prevent API timeout issues
+all_fnames <- unlist(sapply(file_names, extract_fnames), use.names = FALSE)
+all_fnames <- all_fnames[!duplicated(all_fnames) & all_fnames != ""]
+all_fnames <- split(all_fnames, ceiling(seq_along(all_fnames)/200))
 
-for (k in 1:length(temp)) {
-  
-    file <- read_csv(temp[k])
-   
-    ##This part needs manual eyeballing, pick the journal that is your journal of interest, rather than the one with the highest frequency
-    ##Automatically resort to the most frequent name
-    
-    sort(table(file$Journal),decreasing=TRUE)[1:10]
-    
-    #Change to subset to your journal of interest
-    top_name<-names(sort(table(file$Journal),decreasing=TRUE))[1]
-    top_name
-    
-    file <- file %>% filter(Journal == top_name)
-  
-    ##Delete cases without first name
-    
-    file_filtered <- file %>% dplyr::filter(file$FName_1st!="")
-    
-    file_filtered$FName_1st<-as.character(file_filtered$FName_1st)
-    file_filtered$FName_last<-as.character(file_filtered$FName_last)
-    
-    ##Function to Delete Middle Initials
-    ##Delete the shorter component in a first name, seperated by space.  
-    
-    TruncateName<-function(input) {
-      a<-unlist(strsplit(input,split=" "))
-      b<-unlist(lapply(a,nchar))
-      c<-a[which.max(b)]
-      return(c)
-    }
-    
-    ##Truncate the first names of first author
-    file_filtered$FName_1st_Truncate<-NA
-    
-    for (i in 1:length(file_filtered$FName_1st)) {
-      file_filtered$FName_1st_Truncate[i] <- TruncateName(file_filtered$FName_1st[i])
-      print(i)
-    }
-    
-    ##Truncate the first names of last author
-    
-    file_filtered$FName_last_Truncate<-NA
-    
-    for (i in 1:length(file_filtered$FName_last)) {
-      if (is.na(file_filtered$FName_last[i])) {
-        file_filtered$FName_last_Truncate[i] <- "" }
-      else {
-        file_filtered$FName_last_Truncate[i] <- TruncateName(file_filtered$FName_last[i])}
-      print(i)
-    }
-    
-    #First Author
-    file_filtered$given_name_first<-NULL
-    given_name_first = findGivenNames(file_filtered$FName_1st_Truncate,textPrepare=FALSE,apikey="YourAPIKey",progress=TRUE)
-    #Pull all unique first authors
-    given_name_first<-given_name_first[!duplicated(given_name_first),]
-    #Left join to original file
-    colnames(given_name_first)[1]<-"FName_1st_Truncate"
-    file_filtered<-dplyr::left_join(file_filtered,given_name_first,by="FName_1st_Truncate")
-    
-    #Change generic column names to indicate first authorship
-    colnames(file_filtered)[15]<-"Gender_1st"
-    colnames(file_filtered)[16]<-"Probability_1st"
-    colnames(file_filtered)[17]<-"Count_1st"
-    
-    #Last Author
-    file_filtered$given_name_last<-NULL
-    FName_last_Truncate<-dplyr::filter(file_filtered,FName_last_Truncate!="")$FName_last_Truncate
-    given_name_last = findGivenNames(FName_last_Truncate,textPrepare=FALSE,apikey="YourAPIKey",progress=TRUE)
-    #Pull all unique last authors
-    given_name_last<-given_name_last[!duplicated(given_name_last),]
-    #Left join to original file
-    colnames(given_name_last)[1]<-"FName_last_Truncate"
-    file_filtered<-dplyr::left_join(file_filtered,given_name_last,by="FName_last_Truncate")
-    
-    
-    #Change generic column names to indicate last authorship
-    colnames(file_filtered)[18]<-"Gender_last"
-    colnames(file_filtered)[19]<-"Probability_last"
-    colnames(file_filtered)[20]<-"Count_last"
-  
-    write.csv(file_filtered,paste("Processed_",temp[k],sep=""))
+# Genderize first names, convert to data frame
+gender_fnames <- lapply(all_fnames, genderize_fnames)
+gender_fnames <- do.call(rbind, gender_fnames)
+gender_fnames <- gender_fnames[order(gender_fnames$name), ]
 
-}
-
-
+# Save genderized first names data
+save(gender_fnames, 
+     file = file.path(figure_dir, "Genderized_First_Names.RData"))
